2fe1ba85093b3ff6b85c760d20973a69a36da196
Daniel Pinto <apintod@uninorte.edu.co>
2022-08-24 01:11:08 -0500

    Route finish

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -31,6 +33,12 @@ Route::get('/blog/{slug}', function ($slug) {
 
     //Consulta a base de datos
 
-    return 'Listado de publicaciones.';
+    return $slug;
 });
+
+Route::get('buscar', function (Request $request) {
+
+    return $request->all();
+});
+
 
