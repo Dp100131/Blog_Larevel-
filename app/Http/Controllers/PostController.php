7fe8b2cd95b9f4a57ec4cfc34f206177b7310f68
Daniel Pinto <apintod@uninorte.edu.co>
2022-08-24 12:06:16 -0500

    Función eliminar

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return View('posts.index');
+        return View('posts.index', [
+            'posts' => Post::latest()->paginate()
+        ]);
+    }
+
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return back();
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,7 +9,35 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    Listado de publicaciones
+
+                    <table class="mb-4">
+                        @foreach ($posts as $post)
+
+                            <tr class="border-b border-gray-200 text-sm">
+                                <td class="px-6 py-4">{{ $post->title }}</td>
+                                <td class="px-6 py-4">
+                                    <a href="" class="text-indigo-600">Editar</a>
+                                </td>
+                                <td class="px-6 py-4">
+                                    <a href="" class="text-indigo-600">
+                                        <form action="{{ route('posts.destroy', $post) }}" method="post">
+                                            @csrf
+                                            @method('DELETE')
+
+                                            <input
+                                                type="submit"
+                                                value="Eliminar"
+                                                class="bg-gray-800 text-white rounded px-4 py-2"
+                                                onclick="return confirm('Desea eliminar?')">
+                                        </form>
+                                    </a>
+                                </td>
+                            </tr>
+
+                        @endforeach
+                    </table>
+                    {{ $posts->links() }}
+
                 </div>
             </div>
         </div>
