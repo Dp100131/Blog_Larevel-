633eb394827a9b21b46fe8af0593afd809806904
Daniel Pinto <apintod@uninorte.edu.co>
2022-08-24 04:44:05 -0500

    Sistema de inicio de sesion

---
+++ app/Models/Post.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
+
+/**
+ * Comandos:
+ *
+ * php artisan make:model Name -fc// crea el constructor y el factory de una
+ * php artisan migrate:refresh --seed // create fakers post
+ *
+ */
